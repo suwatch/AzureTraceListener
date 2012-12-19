c42deddf92813314d9873f7813d349ec6461dafd
Suwat Ch <suwatch@microsoft.com>
2012-12-19 12:26:03 -0800

    add trace file cleanup

--- AzureTraceListener.cs
+++ AzureTraceListener.cs
@@ -1,80 +1,86 @@
-﻿using System;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Text;
-using System.Web;
+using System.Web.Hosting;
+using System;
 
 namespace AzureTraceListener
 {
     public class AzureTraceListener : TraceListener
     {
+        private DirectoryInfo _directory;
         private string _fileName;
+        private int _traceCount;
 
-        public AzureTraceListener(string fileName)
+        public AzureTraceListener(string path)
         {
-            FileInfo info = new FileInfo(Path.Combine(System.Web.Hosting.HostingEnvironment.MapPath("/"), fileName));
+            _directory = new DirectoryInfo(Path.Combine(HostingEnvironment.MapPath("/"), path));
 
-            _fileName = info.FullName;
-
-            if (!info.Directory.Exists)
+            if (!_directory.Exists)
             {
-                info.Directory.Create();
+                _directory.Create();
+            }
+
+            CleanUpOldFiles();
+
+            EnsureMaxFiles();
+        }
+
+        private void CleanUpOldFiles()
+        {
+            DateTime now = DateTime.UtcNow;
+            foreach (FileInfo file in _directory.GetFiles())
+            {
+                if (now.Subtract(file.LastWriteTimeUtc) > TimeSpan.FromMinutes(10))
+                {
+                    SafeDelete(file);
+                }
+            }
+        }
+
+        private void EnsureMaxFiles()
+        {
+            FileInfo[] files = _directory.GetFiles(Environment.MachineName + "-*.txt")
+                .OrderBy(f => f.LastWriteTimeUtc).ToArray();
+            for (int i = 0; i < files.Length - 3; ++i)
+            {
+                SafeDelete(files[i]);
+            }
+
+            _fileName = Path.Combine(_directory.FullName, String.Format("{0}-{1}.txt", Environment.MachineName, DateTime.UtcNow.Ticks));
+            _traceCount = 0;
+        }
+
+        private void SafeDelete(FileInfo file)
+        {
+            try
+            {
+                file.Delete();
+            }
+            catch (Exception)
+            {
             }
         }
 
         public override void Write(string message)
         {
-            using (FileStream stream = new FileStream(_fileName, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
+            if (++_traceCount > 100)
+            {
+                EnsureMaxFiles();
+            }
+
+            using (FileStream stream = new FileStream(_fileName, FileMode.Append, FileAccess.Write, FileShare.Read))
             {
                 byte[] bytes = Encoding.UTF8.GetBytes(message);
 
                 stream.Write(bytes, 0, bytes.Length);
-
-                stream.Flush();
             }
         }
 
         public override void WriteLine(string message)
         {
-            using (FileStream stream = new FileStream(_fileName, FileMode.Append, FileAccess.Write, FileShare.ReadWrite))
-            {
-                byte[] bytes = Encoding.UTF8.GetBytes(message + "\r\n");
-
-                stream.Write(bytes, 0, bytes.Length);
-
-                stream.Flush();
-            }
+            Write(message + Environment.NewLine);
         }
-
-        //public AzureTraceListener(string fileName)
-        //{
-        //    FileInfo info = new FileInfo(Path.Combine(System.Web.HttpContext.Current.Server.MapPath("/"), fileName));
-        //    _fileName = info.FullName;
-        //    if (!info.Directory.Exists)
-        //    {
-        //        info.Directory.Create();
-        //    }
-
-        //    _stream = new FileStream(_fileName, FileMode.Append, FileAccess.Write, FileShare.ReadWrite);
-        //}
-
-        //public override void Write(string message)
-        //{
-        //    byte[] bytes = Encoding.UTF8.GetBytes(message);
-        //    _stream.Write(bytes, 0, bytes.Length);
-        //    _stream.Flush();
-        //    FlushFileBuffers(_stream.SafeFileHandle.DangerousGetHandle());
-        //}
-
-        //public override void WriteLine(string message)
-        //{
-        //    byte[] bytes = Encoding.UTF8.GetBytes(message + "\r\n");
-        //    _stream.Write(bytes, 0, bytes.Length);
-        //    _stream.Flush();
-        //    FlushFileBuffers(_stream.SafeFileHandle.DangerousGetHandle());
-        //}
-
-        //[DllImport("kernel32", SetLastError = true)]
-        //private static extern bool FlushFileBuffers(IntPtr handle);
     }
 }
